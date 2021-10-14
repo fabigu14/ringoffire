0a186c0c573e14ba4351b3023dd7aa62124b14e6
Fabian Gurth <fabigurth@googlemail.com>
2021-10-14 17:07:53 +0200

    pick animation fix & game model

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -8,13 +9,53 @@ import { Component, OnInit } from '@angular/core';
 export class GameComponent implements OnInit {
 
   pickCardAnimation = false;
+  animationCompleted = true;
+  topCardFlipped = true;
+  game: Game;
+  currentCard: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
   }
 
-  takeCard(){
-    this.pickCardAnimation = true;
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      // console.log(this.currentCard);
+      
+      // console.log(this.game.playedCards);
+      
+      this.pickCardAnimation = true;
+      this.topCardFlipped = false;
+
+      this.flipBackTopCard();
+      setTimeout(() => {
+        
+        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.currentCard);
+        
+      }, 1000);
+    }
+
+  }
+
+  flipBackTopCard(){
+    setTimeout(() => {
+      this.animationCompleted = false;
+      this.topCardFlipped = true;
+      
+      setTimeout(() => {
+        this.animationCompleted = true;
+      }, 500);
+    }, 250);
+    
   }
 }
